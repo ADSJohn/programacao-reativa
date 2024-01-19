2e69f1004b42b9e97df30d87d29f4697e3b621b5
ADSJohn <progjohn@outlook.com>
2024-01-19 15:27:45 -0300

    Start project

---
+++ src/test/java/com/br/boracodardevs/programacaoreativa/ProgramacaoReativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.boracodardevs.programacaoreativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacaoReativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
