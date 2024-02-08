14574ecc50fdf33e17931ae4e7a7f552806dcb7b
ADSJohn <progjohn@outlook.com>
2024-02-08 13:46:43 -0300

    add exception handler to object not found

--- src/main/java/com/br/boracodardevs/programacaoreativa/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/br/boracodardevs/programacaoreativa/exceptions/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.br.boracodardevs.programacaoreativa.exceptions;
 
+import com.br.boracodardevs.programacaoreativa.service.exception.ObjectNotFoundException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -40,6 +43,18 @@ public class ControllerExceptionHandler {
 			error.addError(err.getField(), err.getDefaultMessage());
 		}
 		return ResponseEntity.badRequest().body(Mono.just(error));
+	}
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	ResponseEntity<Mono<StandardError>> objectNotFoundException(ObjectNotFoundException ex, ServerHttpRequest request) {
+		return ResponseEntity.status(NOT_FOUND)
+						.body(Mono.just(StandardError.builder()
+										.timestamp(LocalDateTime.now())
+										.status(HttpStatus.NOT_FOUND.value())
+										.error(HttpStatus.NOT_FOUND.getReasonPhrase())
+										.message(ex.getMessage())
+										.path(request.getPath().toString())
+										.build()));
 	}
 
 	private String verifyDumpKey(String message) {
--- src/main/java/com/br/boracodardevs/programacaoreativa/service/UserService.java
+++ src/main/java/com/br/boracodardevs/programacaoreativa/service/UserService.java
@@ -4,10 +4,13 @@ import com.br.boracodardevs.programacaoreativa.entity.User;
 import com.br.boracodardevs.programacaoreativa.mapper.UserMapper;
 import com.br.boracodardevs.programacaoreativa.model.request.UserRequest;
 import com.br.boracodardevs.programacaoreativa.repository.UserRepository;
+import com.br.boracodardevs.programacaoreativa.service.exception.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +28,7 @@ public class UserService {
 	}
 
 	public Mono<User> findById(final String id) {
-		return repository.findbById(id);
+		return repository.findbById(id)
+						.switchIfEmpty(Mono.error(new ObjectNotFoundException(format("Object not found. Id: %s Type: %s ", id, User.class.getSimpleName()))));
 	}
 }
