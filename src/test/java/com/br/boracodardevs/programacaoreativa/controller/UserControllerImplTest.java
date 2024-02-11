5fcf813bf69e186777989562b50e4059e8c0d6d3
ADSJohn <progjohn@outlook.com>
2024-02-10 22:59:45 -0300

    add integration test to findAll endpoint with success

--- src/test/java/com/br/boracodardevs/programacaoreativa/controller/UserControllerImplTest.java
+++ src/test/java/com/br/boracodardevs/programacaoreativa/controller/UserControllerImplTest.java
@@ -19,6 +19,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @ExtendWith(SpringExtension.class)
@@ -100,7 +101,22 @@ class UserControllerImplTest {
 	}
 
 	@Test
-	void findAll() {
+	void testFindAll() {
+		final var userResponse = new UserResponse(ID, NAME, EMAIL, PASSWORD);
+
+		Mockito.when(service.findAll()).thenReturn(Flux.just(User.builder().build()));
+		Mockito.when(mapper.toResponse(ArgumentMatchers.any(User.class))).thenReturn(userResponse);
+
+		webTestClient.get()
+			.uri("/users")
+			.accept(MediaType.APPLICATION_JSON)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$.[0].id").isEqualTo(ID)
+			.jsonPath("$.[0].name").isEqualTo(NAME)
+			.jsonPath("$.[0].email").isEqualTo(EMAIL)
+			.jsonPath("$.[0].password").isEqualTo(PASSWORD);
 	}
 
 	@Test
